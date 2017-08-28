5224cad9647bd32b657ee68902fbb60db0f45ced
Andrin Bertschi <andrin.bertschi@gmail.com>
2017-08-28 18:04:51 +0200

    wip

--- swypes.py
+++ swypes.py
@@ -448,13 +448,13 @@ class Swypes:
             data = data.strip()
             data = data.replace('\n', '')
 
-            splits = url.split('/')
-            filename = splits[len(splits) - 1]
-            filename = '640x640_' + filename
-            splits[len(splits) - 1] = filename
+            # splits = url.split('/')
+            # filename = splits[len(splits) - 1]
+            # filename = '640x640_' + filename
+            # splits[len(splits) - 1] = filename
+            #
+            # url = "/".join(splits)
 
-            url = "/".join(splits)
-            print(url)
             img = f'<a href="data:text/html,{data}"' \
                   f'><img width="200px" src="{url}" /></a> \n'
             return img
